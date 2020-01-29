cd1d8c84e471990954487d331395eaff46d6492c
imotlist <imotlist@gmail.com>
2020-01-29 10:38:28 +0700

    after rename

--- ios/basecodeTests/basecodeTests.m
+++ ios/basecodeTests/basecodeTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface basecodeTests : XCTestCase
+@interface BaseCodeTests : XCTestCase
 
 @end
 
-@implementation basecodeTests
+@implementation BaseCodeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
